2c25640e74d8b2b16005ef269f6e87c39fc6d7b4
Yaddu <vhermansen2@gmail.com>
2017-11-07 21:03:04 +0100

    Made templates folder, abstracted navbar to templates.

---
+++ templates/navbar.php
@@ -0,0 +1,50 @@
+<header>
+  <div class="jumbotron navbar navbar-expand-md navbar-light fixed-top mt-md-3" id="headerJumbo">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><h2>Bedrift</h2></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="test.php"></a>
+          </li>
+        </ul>
+        <form class="form-inline mr-auto">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" size="40">
+          <button class="btn btn-info" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+        </form>
+        <ul class="navbar-nav navbar-right">
+          <li class="nav-item">
+            <a class="nav-link" href="cart.php"><img src="images/cart.jpg" class="img-fluid" id="imgCart"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <nav class="navbar navbar-light fixed-top navbar-expand-md d-none d-md-block">
+    <div class="container">
+      <div class="navbar-nav ml-auto">
+        <div class="btn-group mr-4">
+          <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            User
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Log in</a>
+            <a class="dropdown-item" href="#">Register</a>
+          </div>
+        </div>
+        <div class="btn-group">
+          <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Contact
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Item 1</a>
+            <a class="dropdown-item" href="#">Item 2</a>
+          </div>
+        </div>
+      </ul>
+    </div>
+  </nav>
+</header>
