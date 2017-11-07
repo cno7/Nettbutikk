74ffeb26adf3bdd40c23e26a183ec810744c66a5
Jørgen Ringstad <jurg771@gmail.com>
2017-11-07 22:56:00 +0100

    Working on footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,40 @@
-<footer>
-  <div class="container-fluid bg-3 text-center">
+<footer class="footer">
+  <div class="container bg-3 text-center">
     <div class="row">
-      <div class="col-sm-4">
-        <h3>Snarveier</h3>
+      <div class="col-sm-3">
+        <img src="images/kollb.png" alt="KollB logo" height="91" width="323" />
       </div>
-      <div class="col-sm-4">
-        <h3>Informasjon</h3>
+      <div class="col-sm-3">
+        <ul class="" style="list-style-type:none">
+          <li><h3>SNARVEIER</h3></li>
+          <li><a href="#">Min side</a></li>
+          <li><a href="#">Ordre</a></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+        </ul>
       </div>
-
-      <div class="col-sm-4">
-        <h3>Support</h3>
+      <div class="col-sm-3">
+        <ul class="" style="list-style-type:none">
+          <li><h3>INFORMASJON</h3></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-3">
+        <ul class="" style="list-style-type:none">
+          <li><h3>SUPPORT</h3></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+          <li><a href="#">Blabla</a></li>
+        </ul>
       </div>
     </div>
+  <div class="col-sm-12">
+    <p>KollB Services. B.R.A. Veien 6B. 1783 Halden. Tel 45454545</p>
+    <p>Copyright 2017 KollB.no. All rights reserved.</p>
+  </div>
   </div>
 </footer>
