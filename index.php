9f7bae37b51a8e061b66613212b49d03f129f533
Yaddu <vhermansen2@gmail.com>
2017-11-07 21:38:03 +0100

    fixed spacing

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
   </head>
   <body>
     <?php include 'templates/navbar.php'; ?>
-    <main class="container">
+    <main class="container mt-5">
+      
     </main>
     <!--Scripts-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
