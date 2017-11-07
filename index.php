da2c0a1ad0d22d0d266fc810dfa68820077ff8d6
Yaddu <vhermansen2@gmail.com>
2017-11-07 21:40:20 +0100

    Fixed spacing again

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
   </head>
   <body>
     <?php include 'templates/navbar.php'; ?>
-    <main class="container mt-5">
-      
+    <main class="container">
+
     </main>
     <!--Scripts-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
