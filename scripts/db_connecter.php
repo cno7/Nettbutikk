c14b74fbc146144dc2d0fc4d7cb8d313c213ceb2
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-11-12 20:09:47 +0100

    added user id, renamed database,some small fixes

--- scripts/db_connecter.php
+++ scripts/db_connecter.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'webshop_users_db');
+define('DB_NAME', 'webshop_db');
 
 //Connecting to database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- scripts/reg_to_db.php
+++ scripts/reg_to_db.php
@@ -9,21 +9,37 @@
   $phone = $_POST['phone'];
   $email = $_POST['email'];
   $pwd = $_POST['pwd'];
-
+  $uGroup = 1;
+  $randomNbr = mt_rand( 10000000,99999999 );
+  $id = $randomNbr;
+  /*
+  checkId($id);
+  */
   $sql_check = "SELECT * FROM users WHERE email='$email'";
-
   $result_check = mysqli_query($link, $sql_check);
   if(mysqli_fetch_assoc($result_check)){
-  echo "Email allready in use";
+    echo "Email allready in use";
   }
   else {
-  $hash_pwd = password_hash($pwd, PASSWORD_BCRYPT);
-  $sql = "INSERT INTO `users`(`fname`, `lname`, `address`, `zipcode`, `city`, `phone`, `email`, `pwd`) VALUES ('$fname', '$lname', '$address', '$zipcode', '$city', '$phone', '$email', '$hash_pwd')";
-  $result = mysqli_query($link, $sql);
+    $hash_pwd = password_hash($pwd, PASSWORD_BCRYPT);
+    $sql = "INSERT INTO `users`(`fname`, `lname`, `address`, `zipcode`, `city`, `phone`, `email`, `pwd`, `usergroup`, `id`) VALUES ('$fname', '$lname', '$address', '$zipcode', '$city', '$phone', '$email', '$hash_pwd', $uGroup, $id )";
+    $result = mysqli_query($link, $sql);
   }
 
 
 
+/*id functions*/
+  /*function checkId(){
+    $sql_check = "SELECT * FROM users WHERE id='$id'"
+    $result_check = mysqli_query($link, $sql_check);
+    if(mysqli_fetch_assoc($result_check)){
+    rollId();
+    }
+  }
+  function rollId(){
+    $id = mt_rand(100000000000, 999999999999);
+    checkId();
+  }*/
 
 
 
