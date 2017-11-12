69a88de5e89b449936d2c0ca513795d0ad713717
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-11-12 23:19:00 +0100

    fixed Id adder

--- scripts/reg_to_db.php
+++ scripts/reg_to_db.php
@@ -10,11 +10,7 @@
   $email = $_POST['email'];
   $pwd = $_POST['pwd'];
   $uGroup = 1;
-  $randomNbr = mt_rand( 10000000,99999999 );
-  $id = $randomNbr;
-  /*
-  checkId($id);
-  */
+
   $sql_check = "SELECT * FROM users WHERE email='$email'";
   $result_check = mysqli_query($link, $sql_check);
   if(mysqli_fetch_assoc($result_check)){
@@ -22,25 +18,9 @@
   }
   else {
     $hash_pwd = password_hash($pwd, PASSWORD_BCRYPT);
-    $sql = "INSERT INTO `users`(`fname`, `lname`, `address`, `zipcode`, `city`, `phone`, `email`, `pwd`, `usergroup`, `id`) VALUES ('$fname', '$lname', '$address', '$zipcode', '$city', '$phone', '$email', '$hash_pwd', $uGroup, $id )";
+    $sql = "INSERT INTO `users`(`fname`, `lname`, `address`, `zipcode`, `city`, `phone`, `email`, `pwd`, `usergroup`) VALUES ('$fname', '$lname', '$address', '$zipcode', '$city', '$phone', '$email', '$hash_pwd', '$uGroup')";
     $result = mysqli_query($link, $sql);
   }
-
-
-
-/*id functions*/
-  /*function checkId(){
-    $sql_check = "SELECT * FROM users WHERE id='$id'"
-    $result_check = mysqli_query($link, $sql_check);
-    if(mysqli_fetch_assoc($result_check)){
-    rollId();
-    }
-  }
-  function rollId(){
-    $id = mt_rand(100000000000, 999999999999);
-    checkId();
-  }*/
-
 
 
  ?>
