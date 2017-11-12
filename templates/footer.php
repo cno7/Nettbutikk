4970eb0e62a2e3b64cf57a7d2e7b49d574c417bd
Jørgen Ringstad <jurg771@gmail.com>
2017-11-12 18:18:45 +0100

    Fixed support for small units

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,10 @@
 <footer class="footer">
-  <div class="container bg-3 text-center">
+  <div class="container bg-4 text-center">
     <div class="row">
-      <div class="col-sm-3">
+      <div class="col-lg-3 col-xs-3">
         <img src="images/kollb.png" alt="KollB logo" height="91" width="323" />
       </div>
-      <div class="col-sm-3">
+      <div class="col-lg-3 col-xs-3">
         <ul class="" style="list-style-type:none">
           <li><h3>SNARVEIER</h3></li>
           <li><a href="#">Min side</a></li>
@@ -13,7 +13,7 @@
           <li><a href="#">Blabla</a></li>
         </ul>
       </div>
-      <div class="col-sm-3">
+      <div class="col-lg-3 col-xs-3">
         <ul class="" style="list-style-type:none">
           <li><h3>INFORMASJON</h3></li>
           <li><a href="#">Blabla</a></li>
@@ -22,7 +22,7 @@
           <li><a href="#">Blabla</a></li>
         </ul>
       </div>
-      <div class="col-sm-3">
+      <div class="col-lg-3 col-xs-3">
         <ul class="" style="list-style-type:none">
           <li><h3>SUPPORT</h3></li>
           <li><a href="#">Blabla</a></li>
@@ -32,7 +32,7 @@
         </ul>
       </div>
     </div>
-  <div class="col-sm-12">
+  <div class="col-xs-3">
     <p>KollB Services. B.R.A. Veien 6C. 1783 Halden. Tel 45454545</p>
     <p>Copyright 2017 KollB.no. All rights reserved.</p>
   </div>
