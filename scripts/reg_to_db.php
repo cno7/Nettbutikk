194901b1530b51a8d1a2408190b72c125ccec49d
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-11-13 09:38:45 +0100

    added comments
    added comments where i felt it was needed

--- scripts/reg_to_db.php
+++ scripts/reg_to_db.php
@@ -1,6 +1,6 @@
 <?php
   require_once 'db_connecter.php';
-
+  /*saves variables with the entered value from user_registration.php*/
   $fname = $_POST['fname'];
   $lname = $_POST['lname'];
   $address = $_POST['address'];
@@ -10,7 +10,7 @@
   $email = $_POST['email'];
   $pwd = $_POST['pwd'];
   $uGroup = 1;
-
+  /* Checks the email entered by user.If used, the program will write an error message. if not, it saves the user in the database*/
   $sql_check = "SELECT * FROM users WHERE email='$email'";
   $result_check = mysqli_query($link, $sql_check);
   if(mysqli_fetch_assoc($result_check)){
