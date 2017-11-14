dc5b78961c5d77511333bd2790f04d55cd605189
Yaddu <vhermansen2@gmail.com>
2017-11-14 20:38:25 +0100

    Fixes for mobile

--- productlist.php
+++ productlist.php
@@ -30,20 +30,18 @@
       <h1>Products</h1>
       <div class="container products-row py-2">
         <div class="row">
-          <div class="col-2 col-sm-2">
-            <img src="images/product_placeholder.jpg" class="img-fluid img-thumbnail">
+          <div class="col-4 col-sm-3 col-md-2 pr-0 pr-sm-1">
+            <img src="images/product_placeholder.jpg" class="img-thumbnail img-products">
           </div>
-          <div class="col-sm-6 col-md-8">
+          <div class="col-4 col-sm-5 col-md-8 pl-0 pl-sm-1">
             <h3>Eple</h3>
-            <p class="products-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis rhoncus risus at tristique.</p>
-            <br>
+            <p class="products-description text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis rhoncus risus at tristique.</p>
             <p>Stock: 10stk</p>
           </div>
-          <div class="col-sm-4 col-md-2 ml-auto">
-            <br>
+          <div class="col-4 col-sm-4 col-md-2">
             <h4>100,-</h4>
             <form action="#" method="post">
-              <input type="number" name="quantity" value="1" class="form-control form-control-sm col-sm-6 col-lg-4" />
+              <input type="number" name="quantity" value="1" class="form-control form-control-sm col-8 col-sm-6 col-lg-4" />
               <button type="submit" class="btn btn-danger mt-2 btn-sm">Add to cart</button>
             </form>
           </div>
@@ -55,6 +53,8 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="jq.js">
+    </script>
     <?php include 'templates/footer.php'; ?>
   </body>
 </html>
