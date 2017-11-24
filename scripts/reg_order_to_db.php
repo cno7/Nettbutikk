7eb16cc57b8d2d725fa561d4a54b7b5b2a3c50e5
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-11-24 21:07:16 +0100

    registering orders now working as intended

--- scripts/reg_order_to_db.php
+++ scripts/reg_order_to_db.php
@@ -3,22 +3,23 @@ session_start();
 require_once 'db_connecter.php';
 
 if(!isset($_SESSION['id'])){
-  header("Location: index.php");
+  header("Location: ../index.php");
 }
   $userId = $_SESSION['id'];
   $date =  date('Y/m/d H:i:s');
   $total=0;
-  $test=array();
+  $item_array=array();
 
 for ($i=0; $i < sizeof($_SESSION['cart']); $i++) {
   $total+=$_SESSION['cart'][$i][4];
-  array_push($test, implode($_SESSION['cart'][$i], "$"));
+  array_push($item_array, implode($_SESSION['cart'][$i], "$"));
 }
-$items = implode($test, "€");
-echo $items;
-  $sql ="INSERT INTO `orders`(`user_id`, `date`, `items`) VALUES ('$userId', '$date', '$items')";
-  $result = mysqli_query($link, $sql);
+$items = implode($item_array, "€");
 
+  $sql ="INSERT INTO `orders`(`user_id`, `date`, `items`, `total_price`) VALUES ('$userId', '$date', '$items', '$total')";
+  $result = mysqli_query($link, $sql);
+  unset($_SESSION["cart"]);
+  header("Location: ../index.php");
 /*$query = "SELECT * FROM my_tbl_name WHERE array_data LIKE '%value3%'"; søker etter item 3 i arrayen*/
 /*$array = explode("€", $array_data); gjør om stringen til en array*/
  ?>
