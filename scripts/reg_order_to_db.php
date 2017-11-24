c44c7fe4aeb84e96e945438a71c3e959b1b573de
Yaddu <vhermansen2@gmail.com>
2017-11-24 20:56:31 +0100

    Added session to cart, fixed reg_order_to_db

--- scripts/reg_order_to_db.php
+++ scripts/reg_order_to_db.php
@@ -2,24 +2,23 @@
 session_start();
 require_once 'db_connecter.php';
 
-if(isset($_SESSION['id'])){
-  /*$user = $_SESSION['email'];*/
-
+if(!isset($_SESSION['id'])){
+  header("Location: index.php");
+}
   $userId = $_SESSION['id'];
   $date =  date('Y/m/d H:i:s');
   $total=0;
+  $test=array();
 
 for ($i=0; $i < sizeof($_SESSION['cart']); $i++) {
   $total+=$_SESSION['cart'][$i][4];
-  implode($_SESSION['cart'][$i], "$");
+  array_push($test, implode($_SESSION['cart'][$i], "$"));
 }
-implode($_SESSION['cart'], "€");
-$items = $_SESSION['cart'];
-
+$items = implode($test, "€");
+echo $items;
   $sql ="INSERT INTO `orders`(`user_id`, `date`, `items`) VALUES ('$userId', '$date', '$items')";
   $result = mysqli_query($link, $sql);
-}
-header("Location: ../index.php");
+
 /*$query = "SELECT * FROM my_tbl_name WHERE array_data LIKE '%value3%'"; søker etter item 3 i arrayen*/
 /*$array = explode("€", $array_data); gjør om stringen til en array*/
  ?>
